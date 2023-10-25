9941cdd7d6336fc8bf394ba083bd24831f13dab6
sarajadidi <sararivkajadidi@gmail.com>
2023-10-25 11:29:38 -0700

    fixed the fizzbuzz issue

--- using System;.cs
+++ using System;.cs
@@ -4,29 +4,29 @@ class Fizzbuzz
 {
     public string AcceptsNumber(int x)
     {
-            
-            if( x % 3 == 0)
-            {
-                return "fizz";
-            }
-            else if(x % 5 == 0)
-            {
-                return "buzz";
-            }
-            else if(x % 3 == 0 && x % 5 == 0)
-            {
-                return "fizzbuzz";
-            }
-            else
-            {
-                return "No fizz and no buzz!";
-            }            
+
+        if (x % 3 == 0 && x % 5 == 0)
+        {
+            return "fizzbuzz";
+        }
+        else if (x % 3 == 0)
+        {
+            return "fizz";
+        }
+        else if (x % 5 == 0)
+        {
+            return "buzz";
+        }
+        else
+        {
+            return "No fizz and no buzz!";
+        }
     }
     public static void Main()
     {
         Fizzbuzz fizzbuzz = new Fizzbuzz();
         Console.WriteLine("Enter a number: ");
-        int number = int.Parse(Console.Readline());
+        int number = int.Parse(Console.ReadLine());
 
         string result = fizzbuzz.AcceptsNumber(number);
         System.Console.WriteLine(result);
